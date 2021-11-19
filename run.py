2d58d6f0f5b1a7b48ced20dac493b005ae6c7052
Negabinary <negabinary@gmail.com>
2021-11-19 18:55:08 +0000

    Delete previous tex files on run

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 import os, sys, subprocess
 
+os.chdir("output/latex")
+if os.path.exists(sys.argv[2][:-3] + "pdf"):
+    os.remove(sys.argv[2][:-3] + "pdf")
+if os.path.exists(sys.argv[2]):
+    os.remove(sys.argv[2])
+os.chdir("../..")
 os.system("make")
 os.system("dist/rep2rep " + sys.argv[1])
 os.chdir("output/latex")
